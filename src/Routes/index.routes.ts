223b439a4daf7ea0906aebb10efa7859b816c7f3
William Cardoso <williancard123@gmail.com>
2021-10-15 11:23:06 -0400

    implemented login access

--- src/Routes/index.routes.ts
+++ src/Routes/index.routes.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { auth } from './auth.routes'
 import { classifications } from './classifications.routes'
 import { merchandise } from './merchandise.routes'
 import { config_morgan } from './middlewares/logs.routes'
@@ -10,5 +11,5 @@ routers.use(config_morgan)
 routers.use('/classifications', classifications)
 routers.use('/merchandise', merchandise)
 routers.use('/users', users)
-
+routers.use('/auth', auth )
 export {routers}
--- src/Routes/users.routes.ts
+++ src/Routes/users.routes.ts
@@ -3,5 +3,6 @@ import { CreateUserControllerIns } from '../UseCase/Create/CreateUserUseCase'
 import { MiddlewareValidadorEmptyUndefined } from './middlewares/validator.routes'
 const users = express.Router()
 
-users.post('/', MiddlewareValidadorEmptyUndefined, async (req, res) => CreateUserControllerIns.CreateUserController(req, res) )
+users.post('/', MiddlewareValidadorEmptyUndefined, async (req, res) => CreateUserControllerIns.CreateUserController(req, res))
+
 export {users}
